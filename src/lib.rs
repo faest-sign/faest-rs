79e12d55713099600d4c26e3048d41f9fa479466
Lennart Braun <lenerd@posteo.de>
2023-02-12 13:27:06 +0100

    put common types in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,19 @@ mod primitives;
 mod traits;
 pub mod veccom;
 pub mod voleith;
+
+pub use faest::{keygen, PublicKey, SecretKey};
+
+use blake3;
+
+type VC = veccom::GgmVecCom<
+    common::Block128,
+    primitives::Aes128CtrLdPrg,
+    blake3::Hasher,
+    primitives::Blake3LE<common::Block128>,
+>;
+pub type FaestProver =
+    faest::FaestProver<homcom::HomCom128SenderFromVitH<voleith::VoleInTheHeadSenderFromVC<VC>>>;
+pub type FaestVerifier = faest::FaestVerifier<
+    homcom::HomCom128ReceiverFromVitH<voleith::VoleInTheHeadReceiverFromVC<VC>>,
+>;
